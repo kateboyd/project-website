233262e32404fcb116a62614a146aeda12be13ea
Kate <kate.wileman@hotmail.co.uk>
2024-04-17 12:43:35 +0100

    lunch - js working

--- index.js
+++ index.js
@@ -28,31 +28,30 @@ fetch("index.json")
         </div>
         <div id="proj-desc${id}" class="w-1/2 proj-desc content-center p-20 text-wrap">
             <h3 class="pb-10 text-xl"><strong>${project.name}</strong></h3>
-            <button id="button${id}" class="details-button">Click for more details</button>
-        </div>
-        </div>
-        `
 
-        const details = document.querySelectorAll('.details-button')
-        details.forEach((button, id) => {
-            button.addEventListener('click', () => { 
-                console.log("click" + id)
-                const test = document.querySelector(`#proj-desc${id}`)
-                console.log(test)
-                document.querySelector(`#proj-desc${id}`).innerHTML += `
-                <div class="w-1/2 content-center p-20 text-wrap">
-                <h3 class="pb-10 text-xl"><strong>${project.name}</strong></h3>
+            <button id="button${id}" class="details-button">Click for more details</button>
+
+            <div class="content-center pl-20 pr-20 text-wrap hidden">
                 <p>${project.description}</p><br>
                 <a href="#">${project.page}</a><br>
-                <a href="#">${project.repo}</a>
-                </div>
-                `
-            })
+                <a href="#">${project.repo}</a><br>
+            </div>
+        </div>
+        </div>
+        `    
+    })
+
+    const details = document.querySelectorAll('.details-button')
+    details.forEach((button, id) => {
+        button.addEventListener('click', () => {
+            console.log("click" + id)
+            document.querySelector(`#proj-desc${id} div`).classList.remove('hidden')
+            button.classList.add('hidden')
         })
-    
     })
 
 })
+
 
 // document.querySelectorAll(".details-button").forEach(details-button, id => {
 //     details-button.addEventListener("click", () => {
