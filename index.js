3f58af3ae3e11b6769b97fe93702f9a82e6a0327
Kate <kate.wileman@hotmail.co.uk>
2024-04-18 11:36:51 +0100

    placeholder projects and mobile responsive

--- index.js
+++ index.js
@@ -20,23 +20,18 @@ fetch("index.json")
     return response.json()
 }).then((data) => {
     data.projectdata.forEach((project, id) => {
-        document.querySelector(".project-container").classList.remove("hidden")
         document.querySelector(".project-container").innerHTML += `
-        <div class="project-container flex flex-col md:flex-row items-center justify-center my-20 gap-10 mx-20">
-        <div class="w-1/2">
-            <img class="" src="${project.image}" alt="json project image">
-        </div>
-        <div id="proj-desc${id}" class="w-1/2 proj-desc content-center p-20 text-wrap">
-            <h3 class="pb-10 text-xl"><strong>${project.name}</strong></h3>
-
-            <button id="button${id}" class="details-button my-auto text-3xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Click for more details</button>
-
-            <div class="content-center pl-20 pr-20 text-wrap hidden">
-                <p>${project.description}</p><br>
-                <p>Check out the live page <a href="${project.page}" target="_blank"><strong class="text-xl">here</strong></a></p><br>
-                <p>GitHub repo <a href="${project.repo}" target="_blank"><strong class="text-xl">here <i class="fa-brands fa-github"></i></strong></a></p><br>
+        <div class="project-container flex flex-col lg:flex-row items-center justify-center lg:my-20 my-5 mx-5 lg:gap-10 lg:p-10 p-5">
+            <img class="lg:w-1/2 w-full justify-center" src="${project.image}" alt="json project image">
+            <div id="proj-desc${id}" class="lg:w-1/2 w-full proj-desc content-center text-wrap lg:py-20 py-5">
+                <h3 class="pb-10 text-3xl"><strong>${project.name}</strong></h3>
+                <button id="button${id}" class="details-button my-auto text-3xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Click for more details</button>
+                <div class="content-center pl-20 pr-20 text-wrap hidden">
+                    <p>${project.description}</p><br>
+                    <p>Check out the live page <a href="${project.page}" target="_blank"><strong class="text-xl">here</strong></a></p><br>
+                    <p>GitHub repo <a href="${project.repo}" target="_blank"><strong class="text-xl">here <i class="fa-brands fa-github"></i></strong></a></p><br>
+                </div>
             </div>
-        </div>
         </div>
         `    
     })
