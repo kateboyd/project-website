e3d3677b7f11fd7c2b0ac9048ed322e2cce30860
Kate <kate.wileman@hotmail.co.uk>
2024-04-18 20:35:57 +0100

    Revert "final commit of the day revert"
    This reverts commit cff4e95ad35eb11c44ccde1c8ea1cd25bcad7caf.

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ fetch("index.json")
 }).then((data) => {
     data.projectdata.forEach((project, id) => {
         document.querySelector(".project-container").innerHTML += `
-        <div class="project-container flex flex-col xl:flex-row items-center align-center justify-center xl:my-20 xl:mx-20 my-5 mx-5 xl:gap-10 xl:p-10 p-5 max-h-1175 items-stretch">
-            <div class="xl:w-1/2 w-full justify-center min-h-350 xl:max-w-700"><img class="mx-auto shadow-2xl" src="${project.image}" alt="${project.name} project image"></div>
-            <div id="proj-desc${id}" class="xl:w-1/2 w-full max-w-700 xl:max-w-1200 proj-desc content-center text-wrap xl:pt-20 xl:pb-10 py-10">
+        <div class="project-container flex flex-col xl:flex-row items-center justify-center xl:my-20 xl:mx-20 my-5 mx-5 xl:gap-10 xl:p-10 p-5 max-h-1175 items-stretch">
+            <div class="xl:w-1/2 w-full justify-center min-h-350 max-w-700"><img class="mx-auto shadow-2xl" src="${project.image}" alt="${project.name} project image"></div>
+            <div id="proj-desc${id}" class="xl:w-1/2 w-full proj-desc content-center text-wrap xl:pt-20 xl:pb-10 py-10">
                 <h3 class="pb-10 text-3xl"><strong>${project.name}</strong></h3>
                 <button id="button${id}" class="details-button my-auto text-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Click for more details</button>
                 <div class="content-center pl-20 pr-20 text-wrap hidden">
