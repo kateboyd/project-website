8a7c12cf759bcd60a134469789864a2ccc6e411f
Kate <kate.wileman@hotmail.co.uk>
2024-04-18 11:53:10 +0100

    favicon and accessibility checks (failed)

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ fetch("index.json")
     data.projectdata.forEach((project, id) => {
         document.querySelector(".project-container").innerHTML += `
         <div class="project-container flex flex-col lg:flex-row items-center justify-center lg:my-20 my-5 mx-5 lg:gap-10 lg:p-10 p-5">
-            <img class="lg:w-1/2 w-full justify-center" src="${project.image}" alt="json project image">
+            <img class="lg:w-1/2 w-full justify-center" src="${project.image}" alt="${project.name} project image">
             <div id="proj-desc${id}" class="lg:w-1/2 w-full proj-desc content-center text-wrap lg:py-20 py-5">
                 <h3 class="pb-10 text-3xl"><strong>${project.name}</strong></h3>
                 <button id="button${id}" class="details-button my-auto text-3xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Click for more details</button>
