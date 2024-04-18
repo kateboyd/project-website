4c227ef70e3bc2b2590b4b8af7cb17afe149a01a
Kate <kate.wileman@hotmail.co.uk>
2024-04-18 17:08:00 +0100

    end of day v2 colour theme new footer

--- index.js
+++ index.js
@@ -21,15 +21,15 @@ fetch("index.json")
 }).then((data) => {
     data.projectdata.forEach((project, id) => {
         document.querySelector(".project-container").innerHTML += `
-        <div class="project-container flex flex-col lg:flex-row items-center justify-center lg:my-20 my-5 mx-5 lg:gap-10 lg:p-10 p-5">
-            <img class="lg:w-1/2 w-full justify-center" src="${project.image}" alt="${project.name} project image">
-            <div id="proj-desc${id}" class="lg:w-1/2 w-full proj-desc content-center text-wrap lg:py-20 py-5">
+        <div class="project-container flex flex-col xl:flex-row items-center justify-center xl:my-20 xl:mx-20 my-5 mx-5 xl:gap-10 xl:p-10 p-5 max-h-1175 items-stretch">
+            <div class="xl:w-1/2 w-full justify-center min-h-350 max-w-700"><img class="mx-auto shadow-2xl" src="${project.image}" alt="${project.name} project image"></div>
+            <div id="proj-desc${id}" class="xl:w-1/2 w-full proj-desc content-center text-wrap xl:pt-20 xl:pb-10 py-10">
                 <h3 class="pb-10 text-3xl"><strong>${project.name}</strong></h3>
                 <button id="button${id}" class="details-button my-auto text-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Click for more details</button>
                 <div class="content-center pl-20 pr-20 text-wrap hidden">
-                    <p>${project.description}</p><br>
-                    <p>Check out the live page <a href="${project.page}" target="_blank"><strong class="text-xl">here</strong></a></p><br>
-                    <p>GitHub repo <a href="${project.repo}" target="_blank"><strong class="text-xl">here <i class="fa-brands fa-github"></i></strong></a></p><br>
+                    <p class="text-xl">${project.description}</p><br>
+                    <p class="text-l">Check out the live page <a href="${project.page}" target="_blank"><strong class="text-xl">here</strong></a></p><br>
+                    <p class="text-l">GitHub repo <a href="${project.repo}" target="_blank"><strong class="text-xl">here <i class="fa-brands fa-github"></i></strong></a></p><br>
                 </div>
             </div>
         </div>
