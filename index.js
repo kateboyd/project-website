36e116976a3ad6292b467acedd7f322343f92af7
Kate <kate.wileman@hotmail.co.uk>
2024-04-19 10:59:49 +0100

    first draft complete & functional

--- index.js
+++ index.js
@@ -21,15 +21,15 @@ fetch("index.json")
 }).then((data) => {
     data.projectdata.forEach((project, id) => {
         document.querySelector(".project-container").innerHTML += `
-        <div class="project-container flex flex-col xl:flex-row items-center justify-center xl:my-20 xl:mx-20 my-5 mx-5 xl:gap-10 xl:p-10 p-5 max-h-1175 items-stretch">
-            <div class="my-auto xl:w-1/2 w-full justify-center min-h-350 max-w-700"><img class="mx-auto shadow-2xl" src="${project.image}" alt="${project.name} project image"></div>
-            <div id="proj-desc${id}" class="shadow-2xl xl:w-1/2 w-full proj-desc content-center text-wrap xl:pt-20 xl:pb-10 py-10">
+        <div class="project-container flex flex-col xl:flex-row items-center justify-center xl:my-20 xl:mx-20 my-5 mx-5 xl:gap-10 xl:p-10 p-5 xl:max-h-450 items-stretch">
+            <div class="my-auto xl:w-1/2 w-full justify-center min-h-350 max-h-450 max-w-700"><img class="mx-auto shadow-2xl" src="${project.image}" alt="${project.name} project image"></div>
+            <div id="proj-desc${id}" class="xl:min-w-900 xl:max-h-490 shadow-2xl xl:w-1/2 w-full proj-desc content-center xl:pt-20 xl:pb-10 py-10 bg-gradient-to-tl from-orange-50 to-orange-100">
                 <h3 class="pb-10 text-3xl"><strong>${project.name}</strong></h3>
-                <button id="button${id}" class="details-button my-auto text-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Click for more details</button>
-                <div class="content-center pl-20 pr-20 text-wrap hidden">
-                    <p class="text-xl">${project.description}</p><br>
-                    <p class="text-l">Check out the live page <a href="${project.page}" target="_blank"><strong class="text-xl">here</strong></a></p><br>
-                    <p class="text-l">GitHub repo <a href="${project.repo}" target="_blank"><strong class="text-xl">here <i class="fa-brands fa-github"></i></strong></a></p><br>
+                <button id="button${id}" class="hover:text-purple-600 details-button my-auto text-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Click for more details</button>
+                <div class="xl:min-w-950 content-center pl-20 pr-20 text-wrap hidden">
+                    <p class="text-xl overflow-y-auto xl:max-h-36">${project.description}</p><br>
+                    <p class="text-lg">Check out the live page <a href="${project.page}" target="_blank"><strong class="text-xl">here</strong></a></p><br>
+                    <p class="text-lg">GitHub repo <a href="${project.repo}" target="_blank"><strong class="text-xl">here <i class="fa-brands fa-github"></i></strong></a></p><br>
                 </div>
             </div>
         </div>
@@ -45,4 +45,12 @@ fetch("index.json")
         })
     })
 
-})
+    document.querySelector(".home-button").addEventListener("click", () => {
+        const resetdetails = document.querySelectorAll('.details-button')
+        resetdetails.forEach((button, id) => {
+                document.querySelector(`#proj-desc${id} div`).classList.add('hidden')
+                document.querySelector(`#button${id}`).classList.remove('hidden')
+            })
+        })
+        })
+    
